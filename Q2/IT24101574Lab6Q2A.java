d0ecb934f8cdd3dcb59647bea3f93cd0b1cff8e2
IT24101574 <it24101574@my.sliit.lk>
2024-08-28 11:43:33 +0530

    Add IT24101574Lab6Q2A.java to Q2

---
+++ Q2/IT24101574Lab6Q2A.java
@@ -0,0 +1,9 @@
+public class IT24101574Lab6Q2A {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 10) {
+            System.out.print(count + " ");
+            count++;
+        }
+    }
+}
